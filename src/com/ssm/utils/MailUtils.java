ce9f8ae2a59d3dec0d8aef5bf174d204dc398b1c
aljqiang <aljqiang@163.com>
2016-01-08 16:18:59 +0800

    新增多邮箱轮流发送邮件功能

--- src/com/ssm/utils/MailUtils.java
+++ src/com/ssm/utils/MailUtils.java
@@ -3,10 +3,7 @@ package com.ssm.utils;
 import org.apache.log4j.Logger;
 
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
@@ -32,7 +29,7 @@ import javax.mail.internet.MimeUtility;
 
 public class MailUtils {
 
-    private static Logger log=Logger.getLogger(MailUtils.class);
+    private static Logger log = Logger.getLogger(MailUtils.class);
 
     /**
      * 邮箱服务器地址
@@ -59,15 +56,28 @@ public class MailUtils {
      */
     private static String nick;
 
+    /**
+     * 邮箱组
+     */
+    private static Map<String, MailInfo> mailInfoMap = new HashMap<String, MailInfo>();
+
     static {
         try {
+            /**单个邮箱发送*/
             // Test Data
-            host = "smtp.163.com";
-            username = "aljqiang";
-            password = "";
-            from = "aljqiang@163.com";
-            nick = "aljqiang";
+//            host = "smtp.163.com";
+//            username = "aljqiang";
+//            password = "Laijq@9023*";
+//            from = "aljqiang@163.com";
+//            nick = "aljqiang";
             // nick + from 组成邮箱的发件人信息
+
+            // 邮箱信息
+            MailInfo mailInfo1 = new MailInfo("smtp.163.com", "aljqiang", "Laijq@9023*", "aljqiang@163.com", "aljqiang");
+            MailInfo mailInfo2 = new MailInfo("192.168.230.238", "laijq", "hold?fish:palm", "laijq@rongrmb.com", "laijq");
+
+            mailInfoMap.put("1", mailInfo1);
+            mailInfoMap.put("2", mailInfo2);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -76,12 +86,13 @@ public class MailUtils {
     /**
      * 发送邮件
      *
-     * @param to    收件人列表，以","分割
+     * @param mi 邮箱地址信息
+     * @param to       收件人列表，以","分割
      * @param subject  标题
      * @param body     内容
      * @param filepath 附件列表,无附件传递null
      */
-    public static boolean sendMail(String to, String subject, String body,
+    public static boolean sendMail(MailInfo mi,String to, String subject, String body,
                                    List<String> filepath) throws AddressException, MessagingException,
             UnsupportedEncodingException {
         // 参数修饰
@@ -94,14 +105,15 @@ public class MailUtils {
         // 创建Properties对象
         Properties props = System.getProperties();
         // 创建信件服务器
-        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.host", mi.getHost());
+        props.put("mail.smtp.port", "25");
         props.put("mail.smtp.auth", "true"); // 通过验证
         // 得到默认的对话对象
         Session session = Session.getDefaultInstance(props, null);
         // 创建一个消息，并初始化该消息的各项元素
         MimeMessage msg = new MimeMessage(session);
-        nick = MimeUtility.encodeText(nick);
-        msg.setFrom(new InternetAddress(nick + "<" + from + ">"));
+        nick = MimeUtility.encodeText(mi.getNick());
+        msg.setFrom(new InternetAddress(mi.getNick() + "<" + mi.getFrom() + ">"));
         // 创建收件人列表
         if (to != null && to.trim().length() > 0) {
             String[] arr = to.split(",");
@@ -143,7 +155,8 @@ public class MailUtils {
                 msg.saveChanges();
                 // 发送信件
                 Transport transport = session.getTransport("smtp");
-                transport.connect(host, username, password);
+                System.out.println("##############邮件开始发送##############");
+                transport.connect(mi.getHost(), mi.getUsername(), mi.getPassword());
                 transport.sendMessage(msg,
                         msg.getRecipients(Message.RecipientType.TO));
                 transport.close();
@@ -161,15 +174,109 @@ public class MailUtils {
     public static void main(String[] args) throws AddressException,
             UnsupportedEncodingException, MessagingException {
         List<String> filepath = new ArrayList<>();
-        filepath.add("D:\\in1.txt");
-        filepath.add("D:\\in2.txt");
+
         try {
-            boolean result=sendMail("aljqiang@aliyun.com,laijq@rongrmb.com", "测试邮件1", "这是一封测试邮件，请勿回复！！！",
-                    filepath);
-            System.out.println(result?"##############邮件发送成功##############":"##############邮件发送失败##############");
-        }catch (Exception e){
+            // 单个邮箱发送
+//            boolean result = MailUtils.sendMail(mailInfoMap.get("2"),"595227518@qq.com,aljqiang@aliyun.com", "测试邮件1", "这是一封测试邮件，请勿回复！！！",
+//                    filepath);
+
+            // 多个邮箱发送
+            boolean result=false;
+            for (int i = 0; i < 2; i++) {
+                if (i < 1) {
+                    filepath.add("D:\\in1.txt");
+                    filepath.add("D:\\in2.txt");
+                    result = MailUtils.sendMail(mailInfoMap.get("2"),"595227518@qq.com,aljqiang@aliyun.com", "测试邮件1", "这是一封测试邮件，请勿回复！！！", filepath);
+                }else{
+                    filepath.add("D:\\in1.txt");
+                    filepath.add("D:\\in2.txt");
+                    result = MailUtils.sendMail(mailInfoMap.get("1"),"595227518@qq.com,aljqiang@aliyun.com", "测试邮件1", "这是一封测试邮件，请勿回复！！！", filepath);
+                }
+                System.out.println(result ? "##############邮件发送成功##############" : "##############邮件发送失败##############");
+            }
+
+        } catch (Exception e) {
             System.out.println("##############邮件发送失败##############");
             e.printStackTrace();
         }
+    }
+}
+
+class MailInfo {
+    /**
+     * 邮箱服务器地址
+     */
+    private String host;
+
+    /**
+     * 发件人
+     */
+    private String username;
+
+    /**
+     * 邮箱密码
+     */
+    private String password;
+
+    /**
+     * 发件人邮箱
+     */
+    private String from;
+
+    /**
+     * 发件人名称
+     */
+    private String nick;
+
+    public MailInfo() {
+
+    }
+
+    public MailInfo(String host, String username, String password, String from, String nick) {
+        this.host = host;
+        this.username = username;
+        this.password = password;
+        this.from = from;
+        this.nick = nick;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
     }
 }
